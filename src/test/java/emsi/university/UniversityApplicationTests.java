ee741c2b214ba48697ec28ae96e2b6bece786024
kenza <kenza.kenouza28@gmail.com>
2025-04-13 00:23:57 +0100

    Initial commit - all files added

---
+++ src/test/java/emsi/university/UniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package emsi.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
